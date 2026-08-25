36cde909cc2ed745fef3efd4ecafc4371cfd8298
brg444 <79601696+brg444@users.noreply.github.com>
2026-08-25 15:50:07 +0800

    Enforce canonical VTXO txid byte order

--- internal/application/vtxo_pending_proof_test.go
+++ internal/application/vtxo_pending_proof_test.go
@@ -35,10 +35,12 @@ func pendingProofForInputs(
 	t.Helper()
 	proofInputs := make([]intent.Input, len(inputs))
 	for i, input := range inputs {
-		var hash chainhash.Hash
-		copy(hash[:], input.Txid)
+		hash, err := chainhash.NewHashFromStr(hex.EncodeToString(input.Txid))
+		if err != nil {
+			t.Fatal(err)
+		}
 		proofInputs[i] = intent.Input{
-			OutPoint:    &wire.OutPoint{Hash: hash, Index: uint32(input.Vout)},
+			OutPoint:    &wire.OutPoint{Hash: *hash, Index: uint32(input.Vout)},
 			Sequence:    wire.MaxTxInSequenceNum,
 			WitnessUtxo: &wire.TxOut{Value: input.ValueSats, PkScript: bytes.Clone(input.Script)},
 		}
